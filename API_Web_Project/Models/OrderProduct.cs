a090b97e2bf33d0b389fa66058b009e301436d66
SalehM909 <saleh.m.alshabibi@gmail.com>
2024-12-16 15:09:22 +0400

    Add OrderProduct

---
+++ API_Web_Project/Models/OrderProduct.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace OrderManagementSystem.Model
+{
+    public class OrderProduct
+    {
+        public int OrderId { get; set; }  // Foreign Key to Order
+
+        public int ProductId { get; set; }  // Foreign Key to Product
+
+        [Required]
+        public int Quantity { get; set; }  // Required, must be > 0
+
+        // Relationships
+        public Order Order { get; set; }
+        public Product Product { get; set; }
+    }
+
+}
