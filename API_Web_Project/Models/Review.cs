0449e8f332a7b3749bfd11dcd7fcb4dde8ae19dd
SalehM909 <saleh.m.alshabibi@gmail.com>
2024-12-16 15:10:02 +0400

    Add Reviw

---
+++ API_Web_Project/Models/Review.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace OrderManagementSystem.Model
+{
+    public class Review
+    {
+        [Key]
+        public int RID { get; set; }  // Primary Key
+
+        public int UserId { get; set; }  // Foreign Key to User
+
+        public int ProductId { get; set; }  // Foreign Key to Product
+
+        [Required]
+        public int Rating { get; set; }  // Required, 1 to 5
+
+        public string Comment { get; set; }  // Optional
+
+        public DateTime ReviewDate { get; set; }
+
+        // Relationships
+        public User User { get; set; }
+        public Product Product { get; set; }
+    }
+
+}
