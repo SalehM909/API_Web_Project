16e0fcfb389c254f5e118efe8a73d1345ddc8dcd
SalehM909 <saleh.m.alshabibi@gmail.com>
2024-12-16 15:06:01 +0400

    Add project files.

---
+++ API_Web_Project/Models/User.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace OrderManagementSystem.Model
+{
+    public class User
+    {
+        [Key]
+        public int UID { get; set; }  // Primary Key
+
+        [Required]
+        public string Name { get; set; }  // Required
+
+        public string Email { get; set; }  // Unique, regex
+
+        public string Password { get; set; }  // Regex
+
+        [Required]
+        public string Phone { get; set; }  // Required
+
+        [Required]
+        public string Role { get; set; }  // Required
+
+        public DateTime CreatedAt { get; set; }  // DateTime
+
+        // Relationships
+        public ICollection<Order> Orders { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+    }
+
+}
